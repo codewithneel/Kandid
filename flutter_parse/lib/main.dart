d465bd0feb8fc10917acd9e3c81da90c6390c508
EdelEB <edeleb@comcast.net>
2022-02-17 16:06:52 -0500

    database connection established

--- flutter_parse/lib/main.dart
+++ flutter_parse/lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
-void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  final keyApplicationId = 'H0TQYPBtcCqOL9NLbfJoQRmrbie4hLwbLzHv5oF8';
-  final keyClientKey = 'qUlu6Qcxl4cmx535TFyIrQqHMnhplrro1vFynCu9';
-  final keyParseServerUrl = 'https://parseapi.back4app.com';
-
-  await Parse().initialize(keyApplicationId, keyParseServerUrl,
-      clientKey: keyClientKey, autoSendSessionId: true);
-
-  var firstObject = ParseObject('FirstClass')
-    ..set(
-        'message', 'Hey ! First message from Flutter. Parse is now connected');
-  await firstObject.save();
-
+void main() {
   runApp(const MyApp());
 }
 
