a2eb34664e27207877e963561c481e5ad84ceefc
ibrahimhz <60887871+ibrahimhz@users.noreply.github.com>
2022-03-30 19:32:55 -0400

    Signupoverflow (#17)
    * testing overflow issue
    
    * overflow in signup and double navbar fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kandid/responsive/mobile_screen_layout.dart';
 import 'package:kandid/templates/feed_screen.dart';
+import 'package:kandid/templates/my_profile.dart';
 import 'package:kandid/templates/personal_info.dart';
 import 'package:kandid/templates/settings_screen.dart';
 import 'package:kandid/templates/signup_screen.dart';
@@ -11,7 +12,7 @@ import 'package:kandid/templates/login_screen.dart';
 import 'package:kandid/utils/colors.dart';
 
 /// Here you can set the template you want rendered (don't delete the TestTemplate) ///
-const TEMPLATE = MobileScreenLayout();
+const TEMPLATE = LoginScreen();
 //const TEMPLATE = LoginScreen();
 //const TEMPLATE = PersonalInfo();
 //const TEMPLATE = testTemplate();
