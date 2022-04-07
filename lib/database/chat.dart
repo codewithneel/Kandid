afae0e757222202db41074d8f516aa2551124538
neeldp <ndp69@njit.edu>
2022-04-07 17:46:25 -0400

    m

--- lib/database/chat.dart
+++ lib/database/chat.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:kandid/database/user.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
@@ -8,25 +7,25 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 /// Creates new Chat entity and returns its objectId
 Future<String?> chatNew(String user_id1, String user_id2) async {
-
-  final chat = ParseObject('Chat')
-    ..set('listUserIds', [user_id1, user_id2]);
+  final chat = ParseObject('Chat')..set('listUserIds', [user_id1, user_id2]);
   await chat.save();
 
   return chat["objectId"];
 }
 
 /// Returns objectId of chat between Username1 and username2. Creates a new Chat if it doesn't exist
-Future<String?> chatGetIdWithUsernames(String username1, String username2) async {
+Future<String?> chatGetIdWithUsernames(
+    String username1, String username2) async {
   String? id1 = await userGetId(username1);
   String? id2 = await userGetId(username2);
-  if (id1 == null || id2 == null) { return null; } //TODO: some sort of error message
+  if (id1 == null || id2 == null) {
+    return null;
+  } //TODO: some sort of error message
 
   return await chatGetIdWithIds(id1, id2);
 }
 
 Future<String?> chatGetIdWithIds(String id1, String id2) async {
-
   try {
     var query = QueryBuilder<ParseObject>(ParseObject('Chat'));
     query.whereArrayContainsAll("listUserIds", [id1, id2]);
@@ -36,8 +35,7 @@ Future<String?> chatGetIdWithIds(String id1, String id2) async {
         return obj["objectId"];
       }
     }
-  }
-  catch (e) {
+  } catch (e) {
     debugPrint("Failed to get ID for Chat between $id1 and $id2\n$e");
   }
   return chatNew(id1, id2);
@@ -56,11 +54,10 @@ Future<List<dynamic>?> chatGetMessages(String chat_id) async {
     final ParseResponse apiResponse = await query.query();
     if (apiResponse.success && apiResponse.results != null) {
       for (ParseObject obj in apiResponse.results!) {
-        ret.add( obj["objectId"] ) ;
+        ret.add(obj["objectId"]);
       }
     }
-  }
-  catch (e) {
+  } catch (e) {
     debugPrint("Failed to get Messages from chat id $chat_id\n$e");
   }
 
@@ -76,14 +73,12 @@ Future<List<String>?> getChats(String user_id) async {
     final ParseResponse apiResponse = await query.query();
     if (apiResponse.success && apiResponse.results != null) {
       for (ParseObject obj in apiResponse.results!) {
-          ret.add(obj["objectId"]);
+        ret.add(obj["objectId"]);
       }
       return ret;
     }
-  }
-  catch (e) {
+  } catch (e) {
     debugPrint("Failed to get ID for Chats for user_id $user_id\n$e");
   }
   return null;
-
 }
